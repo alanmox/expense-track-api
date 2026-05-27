6fd99c4f2eb99fab37e169c8916b897eb8b19d85
alanmox <alanmox441@gmail.com>
2026-05-28 00:43:36 +0300

    console.log

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const testConnection = require("./testConnection");
 const expensesRouter = require("./routes/expenses");
+const logger = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(logger);
 
 app.get("/", (_req, res) => {
   res.json({ message: "Expense Track API" });
