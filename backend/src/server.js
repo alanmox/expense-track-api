522d68a5c283aed6ab892812951ec9da4df10a76
alanmox <alanmox441@gmail.com>
2026-05-30 00:43:59 +0300

    addition of cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,14 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(logger);
 
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (_req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 app.get("/", (_req, res) => {
   res.json({ message: "Expense Track API is running properly..." });
 });
